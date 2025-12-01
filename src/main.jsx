187f56b3ad7d50ec6dc6beddea8798f5de657f21
fizlib <steponas.peteraitis@gmail.com>
2025-12-01 11:41:41 +0200

    Refactor file structure and add nojekyll

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { HashRouter, Routes, Route } from 'react-router-dom'
-import MathHomeworkApp from '../medos.jsx'
-import GeometricHomeworkApp from '../GeometricApp.jsx'
-import LandingPage from '../LandingPage.jsx'
+import MathHomeworkApp from './medos.jsx'
+import GeometricHomeworkApp from './GeometricApp.jsx'
+import LandingPage from './LandingPage.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
