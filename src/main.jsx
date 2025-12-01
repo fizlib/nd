21d1825d44a4c5d657132967b5f99afb08efa4fa
fizlib <steponas.peteraitis@gmail.com>
2025-12-01 11:31:37 +0200

    Switch to HashRouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { HashRouter, Routes, Route } from 'react-router-dom'
 import MathHomeworkApp from '../medos.jsx'
 import GeometricHomeworkApp from '../GeometricApp.jsx'
 import LandingPage from '../LandingPage.jsx'
@@ -8,12 +8,12 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <BrowserRouter basename="/nd">
+        <HashRouter>
             <Routes>
                 <Route path="/" element={<LandingPage />} />
                 <Route path="/aritmetine" element={<MathHomeworkApp />} />
                 <Route path="/geometrine" element={<GeometricHomeworkApp />} />
             </Routes>
-        </BrowserRouter>
+        </HashRouter>
     </React.StrictMode>,
 )
